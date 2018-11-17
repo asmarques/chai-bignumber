9aae4204aa4e8bcdbfbb1be9dd3406bc00f56fc5
António Marques <me@antoniomarques.eu>
2018-11-17 11:22:02 +0000

    Fix compatibility with BigNumber >= 6.0.0 and bump BigNumber to 8.0.1

--- chai-bignumber.js
+++ chai-bignumber.js
@@ -1,6 +1,11 @@
 module.exports = function(BigNumber){
-
   BigNumber = BigNumber || require('bignumber.js');
+  var round = BigNumber.prototype.round || BigNumber.prototype.decimalPlaces;
+  var isEqualTo = BigNumber.prototype.isEqualTo || BigNumber.prototype.equals;
+  var isGreaterThan = BigNumber.prototype.isGreaterThan || BigNumber.prototype.greaterThan;
+  var isGreaterThanOrEqualTo = BigNumber.prototype.isGreaterThanOrEqualTo || BigNumber.prototype.greaterThanOrEqualTo;
+  var isLessThan = BigNumber.prototype.isLessThan || BigNumber.prototype.lessThan;
+  var isLessThanOrEqualTo = BigNumber.prototype.isLessThanOrEqualTo || BigNumber.prototype.lessThanOrEqualTo;
 
   return function (chai, utils) {
     chai.Assertion.addProperty('bignumber', function() {
@@ -29,93 +34,82 @@ module.exports = function(BigNumber){
         if (rm === undefined) {
           rm = BigNumber.ROUND_HALF_UP;
         }
-        number = number.round(dp, rm);
+        number = round.bind(number)(dp, rm);
       }
 
       return number;
     };
 
-    var override = function(fn) {
-      return function (_super) {
-        return function(value, dp, rm) {
-          if (utils.flag(this, 'bignumber')) {
-            var expected = convert(value, dp, rm);
-            var actual = convert(this._obj, dp, rm);
-            fn.apply(this, [expected, actual]);
-          } else {
-            _super.apply(this, arguments);
-          }
-        };
-      };
+    var overwriteMethods = function(names, fn) {
+      for (var i = 0; i < names.length; i++) {
+        chai.Assertion.overwriteMethod(names[i], function (original) {
+          return function(value, dp, rm) {
+            if (utils.flag(this, 'bignumber')) {
+              var expected = convert(value, dp, rm);
+              var actual = convert(this._obj, dp, rm);
+              fn.apply(this, [expected, actual]);
+            } else {
+              original.apply(this, arguments);
+            }
+          };
+        });
+      }
     };
 
-    // BigNumber.equals
-    var equals = override(function(expected, actual) {
+    // BigNumber.isEqualTo
+    overwriteMethods(['equal', 'equals', 'eq'], function(expected, actual) {
       this.assert(
-        expected.equals(actual),
+        isEqualTo.bind(expected)(actual),
         'expected #{act} to equal #{exp}',
         'expected #{act} to be different from #{exp}',
         expected.toString(),
         actual.toString()
       );
     });
-    chai.Assertion.overwriteMethod('equal', equals);
-    chai.Assertion.overwriteMethod('equals', equals);
-    chai.Assertion.overwriteMethod('eq', equals);
 
-    // BigNumber.greaterThan
-    var greaterThan = override(function(expected, actual) {
+    // BigNumber.isGreaterThan
+    overwriteMethods(['above', 'gt', 'greaterThan'], function(expected, actual) {
       this.assert(
-        actual.greaterThan(expected),
+        isGreaterThan.bind(actual)(expected),
         'expected #{act} to be greater than #{exp}',
         'expected #{act} to be less than or equal to #{exp}',
         expected.toString(),
         actual.toString()
       );
     });
-    chai.Assertion.overwriteMethod('above', greaterThan);
-    chai.Assertion.overwriteMethod('gt', greaterThan);
-    chai.Assertion.overwriteMethod('greaterThan', greaterThan);
 
-    // BigNumber.greaterThanOrEqualTo
-    var greaterThanOrEqualTo = override(function(expected, actual) {
+    // BigNumber.isGreaterThanOrEqualTo
+    overwriteMethods(['least', 'gte'], function(expected, actual) {
       this.assert(
-        actual.greaterThanOrEqualTo(expected),
+        isGreaterThanOrEqualTo.bind(actual)(expected),
         'expected #{act} to be greater than or equal to #{exp}',
         'expected #{act} to be less than #{exp}',
         expected.toString(),
         actual.toString()
       );
     });
-    chai.Assertion.overwriteMethod('least', greaterThanOrEqualTo);
-    chai.Assertion.overwriteMethod('gte', greaterThanOrEqualTo);
 
-    // BigNumber.lessThan
-    var lessThan = override(function(expected, actual) {
+    // BigNumber.isLessThan
+    overwriteMethods(['below', 'lt', 'lessThan'], function(expected, actual) {
       this.assert(
-        actual.lessThan(expected),
+        isLessThan.bind(actual)(expected),
         'expected #{act} to be less than #{exp}',
         'expected #{act} to be greater than or equal to #{exp}',
         expected.toString(),
         actual.toString()
       );
     });
-    chai.Assertion.overwriteMethod('below', lessThan);
-    chai.Assertion.overwriteMethod('lt', lessThan);
-    chai.Assertion.overwriteMethod('lessThan', lessThan);
 
-    // BigNumber.lessThanOrEqualTo
-    var lessThanOrEqualTo = override(function(expected, actual) {
+    // BigNumber.isLessThanOrEqualTo
+    overwriteMethods(['most', 'lte'], function(expected, actual) {
       this.assert(
-        actual.lessThanOrEqualTo(expected),
+        isLessThanOrEqualTo.bind(actual)(expected),
         'expected #{act} to be less than or equal to #{exp}',
         'expected #{act} to be greater than #{exp}',
         expected.toString(),
         actual.toString()
       );
     });
-    chai.Assertion.overwriteMethod('most', lessThanOrEqualTo);
-    chai.Assertion.overwriteMethod('lte', lessThanOrEqualTo);
 
     // BigNumber.isFinite
     chai.Assertion.addProperty('finite', function () {
